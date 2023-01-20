9dff69b876dd122dde8f713f647315d6220f5715
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2023-01-20 11:59:42 +0100

    Added script to get folder and files count

---
+++ System/Get-Folder-Files-Count.ps1
@@ -0,0 +1,28 @@
+<#
+
+.SYNOPSIS
+
+Get directories and files count
+
+.EXAMPLE
+
+PS > Get-Folder-Files-Count
+get stats for current folder
+
+PS > Get-Folder-Files-Count -folder "c:\Work"
+get stats for specific folder
+
+#>
+
+param(
+    ## provided folder
+    [Parameter]
+    $folder
+)
+
+if ($folder -eq "") {
+	$folder=Get-Location
+}
+
+$directories,$files = Get-ChildItem $folder -Force -Recurse | Measure-Object -Sum PSIsContainer, Length -ErrorAction Ignore
+$directories,$files
