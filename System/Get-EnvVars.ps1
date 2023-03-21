a62ff54931cea93031094f568b2d45cfc272f8a3
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2023-03-21 13:47:27 +0100

    add function to read and set env files

---
+++ System/Get-EnvVars.ps1
@@ -0,0 +1,29 @@
+<#
+
+.SYNOPSIS
+
+Get and set env files from env file
+
+.EXAMPLE
+
+PS > Get-EnvVars -EnvFile "c:\Work\test.env"
+
+#>
+
+param(    
+    [Parameter(HelpMessage = "File with env variables", Mandatory = $true)]
+    [string]
+    $EnvFile
+)
+
+f (!(Test-Path $EnvFile -PathType Leaf)) {
+    Write-Error "$EnvFile is not a file."
+    return;
+}
+
+Get-Content $EnvFile | ForEach-Object {
+    $name, $value = $_.split('=')
+    Write-Information "$name=$value"
+    Set-Content env:\$name $value
+    Write-Output "Writing $name to environment variable with $value."
+}
