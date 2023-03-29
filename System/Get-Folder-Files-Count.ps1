a2e6899d38d8c3f325507f2a0929948c4e8e4182
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2023-03-29 10:54:49 +0200

    changing the measure process to get info about files and directories

--- System/Get-Folder-Files-Count.ps1
+++ System/Get-Folder-Files-Count.ps1
@@ -9,21 +9,34 @@ Get directories and files count
 PS > Get-Folder-Files-Count
 get stats for current folder
 
-PS > Get-Folder-Files-Count -folder "c:\Work"
+PS > Get-Folder-Files-Count -Folder "c:\Work"
 get stats for specific folder
 
 #>
 
-param(
-    ## provided folder
-    [Parameter]
-    [string]
-    $folder
+param(    
+    [Parameter(HelpMessage="Folder to get stats for")]
+    [string]$Folder
 )
 
-if ($folder -eq "") {
-	$folder=Get-Location
+Write-Host "Reading folder $Folder"
+if ($Folder -eq "") {
+	$Folder=Get-Location
+    Write-Verbose "Input folder is empty, using current folder $Folder"
+}
+# $directories,$files = Get-ChildItem $folder -Force -Recurse | Measure-Object -Sum PSIsContainer, Length -ErrorAction Ignore
+$directories = Get-ChildItem -Path $Folder -Directory -Recurse | Measure-Object -Sum PSIsContainer -ErrorAction Ignore
+$files = Get-ChildItem -Path $Folder -File -Recurse | Measure-Object -Sum Name -ErrorAction Ignore
+Write-Verbose  $directories
+Write-Verbose  $files
+
+$properties = @{
+    "Directories" = $directories.Count -as [int]
+    "Files" = $files.Count -as [int]
 }
 
-$directories,$files = Get-ChildItem $folder -Force -Recurse | Measure-Object -Sum PSIsContainer, Length -ErrorAction Ignore
-$directories,$files
+
+$outputObject = New-Object psobject -Property $properties;
+Write-Verbose "Output object: $outputObject"
+$outputObject | Format-Table -AutoSize
+
