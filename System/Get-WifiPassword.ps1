61dcaf5343a5db2b0a42c81f735a302092e1f2c4
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2023-05-25 19:58:53 +0300

    added method for getting wifi password from local machine

---
+++ System/Get-WifiPassword.ps1
@@ -0,0 +1,62 @@
+<#
+    .SYNOPSIS
+    This function displays all stored WIFI passwords on the client
+    .DESCRIPTION
+    This function gets the output of netsh command and will trim it to display all SSID`s and also the passwords.
+    If a profile has no password it will inform the user.
+
+    Method is based on the following code - all copyrights to the author of this repository: https://github.com/HCRitter/Get-WIFIPassword/blob/master/Get-WIFIPassword.ps1
+
+    .EXAMPLE
+    Get-WIFIPassword
+
+    .LINK
+    https://github.com/HCRitter/Get-WIFIPassword/blob/master/Get-WIFIPassword.ps1
+#>
+function Get-WifiPassword {
+    [CmdletBinding(DefaultParameterSetName = "Network")]
+    param (        
+    )
+    
+    begin {
+        try {
+            # Export all Wifi profiles and collect their XML file paths
+            $netshOutput = $(netsh wlan export profile key=clear)
+            $XmlFilePaths = foreach($line in $netshOutput){
+                if($line -match '\"(.*?\\.*?\.xml)(?=\")'){
+                    $matches[1]
+                }
+            }
+        }
+        catch {
+            Write-Error "Failed to export Wifi profiles: $($_.Exception.Message)"
+            return
+        }
+        
+    }
+    
+    process {
+        foreach ($XmlFilePath in $XmlFilePaths) {
+            try {
+                # Read the XML file and extract the Wifi profile name and password
+                $Xml = [xml](Get-Content -Path $XmlFilePath)
+
+                # Output the profile name, password, and whether the operation succeeded
+                [PSCustomObject]@{
+                    Name = $Xml.WLANProfile.Name
+                    Password = $Password = $Xml.WLANProfile.MSM.Security.SharedKey.KeyMaterial
+                    Succeed = [bool]$Password
+                }
+                # Remove the XML file
+                Remove-Item $XmlFilePath -ErrorAction Stop
+            }
+            catch {
+                Write-Error "Failed to read Wifi profile from '$XmlFilePath': $($_.Exception.Message)"
+            }
+        }
+    }
+    
+    end {
+        
+    }
+}
