a42918c0959be7f91aaeced1e9e1eb357edb4e91
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2023-06-21 12:25:41 +0200

    added uptime script to get information from system

---
+++ System/Get-UpTime.ps1
@@ -0,0 +1,30 @@
+<#
+
+.SYNOPSIS
+
+Get uptime for the system
+
+.EXAMPLE
+
+PS > Get-InstalledSoftware
+get stats for current folder
+
+.EXAMPLE 
+
+PS > Get-UpTime 
+get uptime for the system
+
+.LINK
+https://jeffhicks.substack.com/p/powershell-functions-101
+
+#>
+[CmdletBinding(DefaultParameterSetName = "System")]
+param(   
+)
+
+Write-Verbose "Getting uptime for the system"
+$os = Get-CimInstance -ClassName Win32_OperatingSystem
+Write-Verbose $os
+$currentTime = New-TimeSpan -Start $os.LastBootUpTime -End (Get-Date)
+Write-Verbose $currentTime
+$currentTime
