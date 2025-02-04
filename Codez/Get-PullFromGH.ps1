a86fcd6da8d5c068db88bab5b38cadf48a910118
Bojan Vrhovnik <bojan@vrhovnik.net>
2025-02-04 08:47:46 +0100

    changing logs to be written to file with output from git pull

--- Codez/Get-PullFromGH.ps1
+++ Codez/Get-PullFromGH.ps1
@@ -40,7 +40,6 @@ param(
     [switch]$ShowLog    
 )
 $logPath = "$HOME/Downloads/get-pullfromgh.log"
-Start-Transcript -Path $logPath -Force
 # get path you were in
 $GoToDir = Get-Location
 if ($null -eq $RootFolderPath)
@@ -48,34 +47,41 @@ if ($null -eq $RootFolderPath)
     $RootFolderPath = Get-Location    
 }
 
+Add-Content -Path $logPath -Value "Starting git pull in $RootFolderPath"
+
 # check if RootFolderPath is a directory and exists
 if (-not (Test-Path -Path $RootFolderPath -PathType Container))
 {
     Write-Output "RootFolderPath $RootFolderPath is not a directory or does not exist"
-    Stop-Transcript
+    Add-Content -Path $logPath -Value "RootFolderPath $RootFolderPath is not a directory or does not exist"
     return
 }
 
 Write-Output "Getting list of directories from $RootFolderPath"
+Add-Content -Path $logPath -Value "Getting list of directories from $RootFolderPath"
 $directories = Get-ChildItem -Path $RootFolderPath -Directory
 Write-Output "Found $($directories.Count) directories"
+Add-Content -Path $logPath -Value "Found $($directories.Count) directories"
 $numberOfDirectories = $directories.Count
 
 $directories | ForEach-Object {
     Write-Output "Doing git pull in $($_.FullName)"
-    Set-Location -Path $_.FullName
-    git pull
+    Add-Content -Path $logPath -Value "Doing git pull in $($_.FullName)"
+    Set-Location -Path $_.FullName    
+    git pull | Out-File -FilePath $logPath -Append
     $numberOfDirectories--
     Write-Output "Done git pull in $($_.FullName), $numberOfDirectories directories left"
+    Add-Content -Path $logPath -Value "Done git pull in $($_.FullName), $numberOfDirectories directories left"
 }
 Set-Location -Path $GoToDir
 Write-Output "Running git pull in $($directories.Count) directories, exiting"
-Stop-Transcript
+Add-Content -Path $logPath -Value "Running git pull in $($directories.Count) directories."
+
 if ($ShowLog)
-{
+{     
     Start-Process "notepad" -ArgumentList $logPath 
 }
 else
-{
+{    
     Write-Output "Log file is saved in $logPath"
 }
